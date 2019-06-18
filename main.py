c38c3b9aaa53133c4174b028a7868c83c38e0627
havelhuang <39305014+havelhuang@users.noreply.github.com>
2019-06-18 21:51:15 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sentimentTestSuite import sentimentTrainModel, sentimentGenerateTestSuite
 from mnistTestSuite import mnist_lstm_train, mnist_lstm_test
 from lipoTestSuite import lipo_lstm_train, lipo_lstm_test
 from record import record
-
+import re
 
 def main():
     
@@ -20,6 +20,7 @@ def main():
     parser.add_argument('--threshold_CC', dest='threshold_CC', default='6', help='')
     parser.add_argument('--threshold_GC', dest='threshold_GC', default='0.78', help='')
     parser.add_argument('--symbols_SQ', dest='symbols_SQ', default='2', help='')
+    parser.add_argument('--seq', dest='seq', default='[70,74]', help='')
     parser.add_argument('--mode', dest='mode', default='test', help='')
     parser.add_argument('--minimalTest', dest='minimalTest', default='0', help='')
     parser.add_argument('--output', dest='filename', default='./log_folder/record.txt', help='')
@@ -32,6 +33,8 @@ def main():
     threshold_CC = args.threshold_CC
     threshold_MC = args.threshold_GC
     symbols_SQ = args.symbols_SQ
+    seq = args.seq
+    seq = re.findall(r"\d+\.?\d*", seq)
     TargMetri = args.TargMetri
     CoverageStop = args.CoverageStop
     TestCaseNum = args.TestCaseNum
@@ -61,19 +64,19 @@ def main():
         if mode == 'train': 
             sentimentTrainModel()
         else: 
-            sentimentGenerateTestSuite(r,threshold_CC,threshold_MC,symbols_SQ,TestCaseNum,minimalTest,TargMetri,CoverageStop)
+            sentimentGenerateTestSuite(r,threshold_CC,threshold_MC,symbols_SQ,seq,TestCaseNum,minimalTest,TargMetri,CoverageStop)
 
     elif modelName == 'mnist':
         if mode == 'train':
             mnist_lstm_train()
         else:
-            mnist_lstm_test(r,threshold_CC,threshold_MC,symbols_SQ,TestCaseNum,minimalTest,TargMetri,CoverageStop)
+            mnist_lstm_test(r,threshold_CC,threshold_MC,symbols_SQ,seq,TestCaseNum,minimalTest,TargMetri,CoverageStop)
 
     elif modelName == 'lipo':
         if mode == 'train':
             lipo_lstm_train()
         else:
-            lipo_lstm_test(r,threshold_CC,threshold_MC,symbols_SQ,TestCaseNum,minimalTest,TargMetri,CoverageStop)
+            lipo_lstm_test(r,threshold_CC,threshold_MC,symbols_SQ,seq,TestCaseNum,minimalTest,TargMetri,CoverageStop)
         
     else: 
         print("Please specify a model from {sentiment, mnist, lipo}")
